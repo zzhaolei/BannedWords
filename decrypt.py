b9a973f51ea269159fb49a1e2bd6874c907161dd
Rui <1040457407@qq.com>
2018-04-09 15:49:05 +0800

    fix some bugs

--- decrypt.py
+++ decrypt.py
@@ -5,7 +5,7 @@ import base64
 
 # base64.b64encode()
 
-with open("./pub_banned_words.txt", "a") as f:
-    with open("./encryption_banned_word.txt", "r") as en:
-        for i in en.readlines():
-            f.write(base64.b64decode(i))
+with open("./pub_banned_words.txt", "r",encoding='utf-8') as f:
+    with open("./encryption_banned_word.txt", "a",encoding='utf-8') as en:
+        for i in f.readlines():
+            en.write(base64.b64decode(i).decode('utf-8'))
